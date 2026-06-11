1b945c9d9ce963bf227210dedf2ec745da0b8f2a
Samm-05 <samyakmahatme@gmail.com>
2026-06-11 10:42:01 +0530

    Add new content

---
+++ dashboard/app.py
@@ -0,0 +1,20 @@
+from streamlit_option_menu import option_menu
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI-Powered Adult Income Analytics",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("💰 AI-Powered Adult Income Analytics Platform")
+
+st.markdown("""
+Predict annual income category,
+analyze demographic trends,
+and explore machine learning insights.
+""")
+
+st.success(
+    "Use the left sidebar to navigate through the platform."
+)
