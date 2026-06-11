3f91ae52b839a99ee28dea7e90153936cab0ec50
Samm-05 <samyakmahatme@gmail.com>
2026-06-11 10:47:38 +0530

    Add new content

---
+++ dashboard/pages/explainable.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.title(
+    "🧠 Explainable AI"
+)
+
+st.image(
+    "dashboard/assets/shap_summary.png"
+)
+
+st.markdown("""
+Top factors influencing income:
+
+• Education
+
+• Capital Gain
+
+• Hours Worked
+
+• Marital Status
+
+• Age
+""")
